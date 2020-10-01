7a4aa3c528d6f0152c923bdf843ed1aa88525a11
Raphael Charre <rcharre@oceaneconsulting.com>
2020-10-01 18:24:11 +0200

    jwt token

--- src/store/index.js
+++ src/store/index.js
@@ -3,119 +3,113 @@ import Vuex from 'vuex'
 import axios from 'axios'
 
 Vue.use(Vuex)
-
 export default new Vuex.Store({
-  state: {
-    status: '',
-    user : null,
-    authorities : {},
-    tryRecoverSession: false,
-    teamModal: false
-  },
-  mutations: {
-    auth_request(state){
-      state.status = 'loading'
+    state: {
+        status: localStorage.getItem('status') || '',
+        token: localStorage.getItem('token') || '',
+        loginDate: localStorage.getItem('loginDate') !== null ? JSON.parse(localStorage.getItem('loginDate')) : null,
+        user : localStorage.getItem('user') !== null ? JSON.parse(localStorage.getItem('user')) : null,
+        authorities: localStorage.getItem('authorities') !== null ? JSON.parse(localStorage.getItem('authorities')) : {}
     },
-    auth_success(state, user){
-      state.user = user;
-      for(var authority of user.appRole.authorities){
-        state.authorities[authority.name] = true;
-      }
-      state.status = 'success';
-    },
-    auth_error(state){
-      state.status = 'error';
-    },
-    recover_success(state, user){
-      state.user = user;
-      for(var authority of user.appRole.authorities){
-        state.authorities[authority.name] = true;
-      }
-      state.status = 'success';
-      state.tryRecoverSession = true;
-    },
-    recover_failed(state){
-      state.tryRecoverSession = true;
-    },
-    logout(state){
-      state.status = '';
-      state.user = null;
-      state.authorities = {};
-    },
-  },
-  actions: {
-    login({commit}, authData){
-      var formData = new FormData();
-      formData.set("username", authData.username);
-      formData.set("password", authData.password);
+    mutations: {
+        auth_request(state){
+            state.status = 'loading'
+        },
+        auth_success(state, {token, user}){
+            state.status = 'success'
+            state.token = token
+            state.user = user
+            state.loginDate = new Date()
+            localStorage.setItem('token', state.token)
+            localStorage.setItem('status', 'success')
+            localStorage.setItem('user', JSON.stringify(state.user))
+            localStorage.setItem('loginDate', JSON.stringify(state.loginDate))
 
-      return new Promise((resolve, reject) => {
-        commit('auth_request')
-        axios({
-          url: '/authentication', data: formData,
-          method: 'POST',
-          headers: {'Content-Type': 'multipart/form-data' }
-        })
-        .then(resp => {
-          axios({
-            url: '/users/me',
-            method: 'GET'
-          })
-          .then(userResp => {
-            resolve(resp)
-            commit('auth_success', userResp.data)
-          })
-          .catch(err => {
-            commit('auth_error')
-            reject(err)
-          })
+            for(const authority of user.appRole.authorities){
+                state.authorities[authority.name] = true;
+            }
+            localStorage.setItem('authorities', JSON.stringify(state.authorities))
 
-        })
-        .catch(err => {
-          commit('auth_error')
-          reject(err)
-        })
-      })
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+        },
+        auth_error(state){
+            localStorage.removeItem('loginDate')
+            localStorage.removeItem('user')
+            localStorage.removeItem('token')
+            localStorage.removeItem('authorities')
+            state.user = null
+            state.status = 'error'
+            state.token = null
+            delete axios.defaults.headers.common['Authorization']
+        },
+        logout(state){
+            delete axios.defaults.headers.common['Authorization']
+            state.status = ''
+            state.token = ''
+            state.loginDate = null
+            state.user = null
+            state.authorities = []
+            localStorage.removeItem('loginDate')
+            localStorage.removeItem('user')
+            localStorage.removeItem('token')
+            localStorage.setItem('status', '')
+            localStorage.removeItem('authorities')
+        },
     },
-    recoverSession({commit}){
-      return new Promise((resolve, reject) => {
-        axios({
-          url: '/users/me',
-          method: 'GET'
-        })
-        .then(userResp => {
-          commit('recover_success', userResp.data);
-          resolve();
-        })
-        .catch(err => {
-          commit('recover_failed');
-          reject(err);
-        })
-      })
+    actions: {
+        login({commit}, user){
+            return new Promise((resolve, reject) => {
+                commit('auth_request')
+                axios({
+                    url: '/authentication/login',
+                    data: user,
+                    method: 'POST' })
+
+                    .then(resp => {
+                        const token = resp.data.jwttoken
+                        axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+                        //get current user
+                        axios({
+                            url: '/users/me',
+                            method: 'get' })
+                            .then(respUser=> {
+                                let user = respUser.data
+                                commit('auth_success', {token, user})
+                                resolve(resp)
+                            })
+                            .catch(err=>{
+                                commit('auth_error')
+                                reject(err)
+                            })
+                    })
+
+                    .catch(err => {
+                        commit('auth_error')
+                        reject(err)
+                    })
+            })
+        },
+        logout({commit}){
+            return new Promise((resolve, reject) => {
+                commit('logout')
+                resolve()
+            })
+        },
+
+        refreshToken({commit, state}){
+            axios({
+                url: '/authentication/refresh-token',
+                method: 'GET' })
+                .then(resp => {
+                    const token = resp.data.jwttoken
+                    const user = state.user
+                    commit('auth_success', {token, user})
+                    resolve(resp)
+                })
+        }
     },
-    resetSession({commit}){
-      return new Promise((resolve, reject) => {
-        commit('logout')
-        resolve()
-      })
-    },
-    logout({commit}){
-      return new Promise((resolve, reject) => {
-        axios({
-          url: '/logout',
-          method: 'GET',
-        })
-        .then(resp => {
-          commit('logout')
-          resolve()
-        })
-      })
+    getters : {
+        isLoggedIn: state => !!state.user,
+        authStatus: state => state.status,
     }
-  },
-  modules: {
-  },
-  getters:{
-    isLoggedIn: state => !!state.user,
-    authStatus: state => state.status,
-  },
 })
