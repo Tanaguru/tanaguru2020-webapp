c6edbf4dc6f4b69e89794d3c1cd475e900c418a8
Raphael Charre <rcharre@oceaneconsulting.com>
2020-11-17 15:49:51 +0100

    add version in footer

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,17 @@
 module.exports = {
+    chainWebpack: config => {
+        config
+            .plugin('define')
+            .tap(args => {
+                let v = JSON.stringify(require('./package.json').version)
+                args[0]['process.env']['VERSION'] = v
+                return args
+            })
+    },
     devServer: {
         //proxy: 'http://178.33.132.243:9002',
         //proxy: 'http://localhost:9002'
-      },
+    },
     css: {
       loaderOptions: {
         sass: {
