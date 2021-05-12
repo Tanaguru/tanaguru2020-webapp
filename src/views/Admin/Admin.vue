d7eeb9856fc97623f465fec92501138132f8e5f7
polly <pfremont@tanaguru.com>
2021-05-12 11:42:34 +0200

    missing ponctuation + password symbols in french trad

--- src/views/Admin/Admin.vue
+++ src/views/Admin/Admin.vue
@@ -1,21 +1,21 @@
 <template>
     <main class="wrapper" id="page" role="main">
-		<Tabs @activeTab='activeTab'>
-			<Tab :name="$t('entity.contract.contracts')" selected="true" class="tabs-wrapper">
-				<adminContractList :selected="selectedTab"/>
-			</Tab>
-
-			<div v-if="this.$store.state.auth.user.appRole.name !== 'USER'">
-				<Tab :name="$t('entity.user.users')" class="tabs-wrapper">
-					<adminUserList :selected="selectedTab"/>
+			<Tabs @activeTab='activeTab'>
+				<Tab :name="$t('entity.contract.contracts')" selected="true" class="tabs-wrapper">
+					<adminContractList :selected="selectedTab"/>
 				</Tab>
 
-				<Tab v-if="showReferenceTab"
-					:name="$t('entity.reference.references')" class="tabs-wrapper">
-					<reference-tab :selected="selectedTab"/>
-				</Tab>
-			</div>
-		</Tabs>
+				<div v-if="this.$store.state.auth.user.appRole.name !== 'USER'">
+					<Tab :name="$t('entity.user.users')" class="tabs-wrapper">
+						<adminUserList :selected="selectedTab"/>
+					</Tab>
+
+					<Tab v-if="showReferenceTab"
+						:name="$t('entity.reference.references')" class="tabs-wrapper">
+						<reference-tab :selected="selectedTab"/>
+					</Tab>
+				</div>
+			</Tabs>
     </main>
 </template>
 
