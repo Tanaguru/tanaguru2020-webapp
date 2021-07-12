810b4010a995a9d252fc5ce3f22f89d5e5f5977f
lpedrau <lpedrau@oceaneconsulting.com>
2021-07-12 15:43:38 +0200

    add possibility to redirect to external module on homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,16 @@ export default {
             // if no subcomponents specify a metaInfo.title, this title will be used
             title: this.$i18n.t("global.siteName") + ' - ' + this.$i18n.t("title.home"),
         }
+    },
+    created(){
+        if(!this.$store.state.auth.user){
+            var modules = this.$store.getters.getOfflineModules;
+            for(module of modules){
+                if(module.home == true){
+                    this.$router.push('/external-module/'+module.name)
+                }
+            }
+        }
     }
 }
 </script>
