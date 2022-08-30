1c75dbfb0229461305ba1fb0c35358b0f7e04ad1
severineFF <sfoucherferet@tanaguru.com>
2022-08-30 18:23:25 +0200

    add conformity rate & nb anomalies on pageList

--- src/views/AuditDetail/PageList.vue
+++ src/views/AuditDetail/PageList.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
-        <div role="list" class="list-pages" v-if="audit && pages.length > 0">
-            <p role="listitem" class="list-pages__item" v-for="(page, i) of pages" :key="page.id">
+        <div role="list" class="list-pages" v-if="audit && pages.length > 0 && pages.length === pagesResult.length">
+            <p role="listitem" class="list-pages__item  list-pages__item-overflow" v-for="(page, i) of pagesResult" :key="page.id">
                 <span class="list-pages__number">{{minElementIndex + i}}.</span>
-				<router-link class="link-simple" :to="'/audits/' + audit.id + '/pages/' + page.id + (audit.sharecode ? '/' + audit.sharecode : '')">{{ page.name }} ({{page.url}})</router-link>
+                <router-link class="link-simple" :to="'/audits/' + audit.id + '/pages/' + page.id + (audit.sharecode ? '/' + audit.sharecode : '')">
+                    {{ page.percentage }}% / {{ page.nbAnomaly }} <span v-if="page.nbAnomaly > 1">{{$t('auditDetail.pageList.anomalies')}}</span><span v-else>{{$t('auditDetail.pageList.anomaly')}}</span> / {{ page.name }}
+                </router-link>
             </p>
         </div>
     </div>
@@ -13,6 +15,39 @@
 export default {
     name: "pageList",
     props: ['audit', 'pages', 'totalElements', 'totalPages', 'currentPage', 'elementByPage'],
+    data() {
+        return {
+            pagesResult: []
+        }
+    },
+    watch: { 
+      	pages: function(newVal, oldVal) {
+            var oldId = oldVal.map(el => el.id);
+            var newId = newVal.map(el => el.id);
+            var pagesHasChanged = newId.filter(e => !oldId.includes(e)).length > 0;
+            if(pagesHasChanged) {
+                this.pages.forEach(page => this.loadPagesStatusResult(page));
+            }
+        }
+    },
+    methods: {
+        loadPagesStatusResult(page) {
+            this.statusResultService.findMainStatusResultByPage(
+                page.id,
+                this.sharecode,
+                (results) => {
+                    Object.assign(page, {
+                        percentage: Math.round((results.nbTP / (results.nbTP + results.nbTF)) * 100),
+					    nbAnomaly: results.nbEF
+                    });
+                    this.pagesResult.push(page);
+                },
+                (error) => {
+                    console.error(error);
+                }
+            )
+        }
+    },
     computed: {
         minElementIndex(){
             return this.currentPage * this.elementByPage + 1;
@@ -23,11 +58,14 @@ export default {
 
 <style lang="scss" scoped>
 .list-pages {
-	margin: 0 0 2.4rem 3.5rem;
-	text-indent: -3.5rem;
+	margin: 0 0 2.4rem 0;
+	// text-indent: -3.5rem;
 
 	.list-pages__item {
 		margin: 0 0 .5rem;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
 	}
 
 	.list-pages__number {
